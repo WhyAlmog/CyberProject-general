ffe070cc2c54c188e48a2e67e2dd3dfaa9af5ba4
Almog <whyalmog@gmail.com>
2021-01-05 12:26:10 +0200

    moved Net to a separate file

--- Network.py
+++ Network.py
@@ -1,39 +1,17 @@
 import os
 
 import torch
-import torch.nn.functional as F
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
+from Net import Net
+
 TRAIN_PATH = "D:\\Datasets\\CyberProject\\"
 MODEL_PATH = "./network.pth"
 EPOCHS = 3
 DEVICE = None
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # https://stackoverflow.com/questions/53784998/how-are-the-pytorch-dimensions-for-linear-layers-calculated
-        self.fc1 = nn.Linear(16 * 22 * 22, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 3)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 22 * 22)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x
 
 
 def main():
